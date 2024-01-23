fc602995e936cc0d0513dac1d0606e09c86eea83
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-01-24 00:46:07 +0100

    Add @next/bundle-analyzer

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,10 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   transpilePackages: ["@repo/ui"],
 };
+
+module.exports = withBundleAnalyzer(nextConfig);
