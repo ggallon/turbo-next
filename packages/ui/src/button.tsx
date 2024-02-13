11ea00239cf814200549ff5d8734e41be7d4dc19
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-02-13 01:58:55 +0100

    Run lint

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -6,11 +6,19 @@ interface ButtonProps {
   className?: string;
 }
 
-export function Button({ appName, children, className }: ButtonProps) {
+export function Button({
+  appName,
+  children,
+  className,
+}: ButtonProps): JSX.Element {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={() => {
+        /* eslint-disable-next-line -- no-alert -- Only for test*/
+        alert(`Hello from your ${appName} app!`);
+      }}
+      type="button"
     >
       {children}
     </button>
